7cd51b22e674e67d706a56477010b06d8aa72c39
Kyle Garcia <kyle@jointmedias.com>
2025-09-06 07:30:47 -0700

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,47 @@
-import Script from 'next/script';
-import SmoothScrolling from '@/components/SmoothScrolling';
-import { ViewTransitions } from 'next-view-transitions';
+import Script from "next/script";
+import SmoothScrolling from "@/components/SmoothScrolling";
+import { ViewTransitions } from "next-view-transitions";
 
-import type { Metadata } from 'next';
-import { Poppins, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Poppins, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const poppinsSans = Poppins({
-  variable: '--font-poppins-sans',
-  weight: ['400', '500', '600', '700'],
-  subsets: ['latin'],
+	variable: "--font-poppins-sans",
+	weight: ["400", "500", "600", "700"],
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Kyle Garcia - Full-Stack Web Developer',
-  description:
-    'Full-stack web developer with a passion for building beautiful and functional web applications.',
+	title: "Kyle Garcia - Full-Stack Web Developer",
+	description:
+		"Full-stack web developer with a passion for building beautiful and functional web applications.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ViewTransitions>
-      <html lang="en">
-        <body
-          className={`${poppinsSans.variable} ${geistMono.variable} antialiased bg-deep-charcoal bg-dither-2 text-off-white`}
-        >
-          <SmoothScrolling>{children}</SmoothScrolling>
-        </body>
-        <Script
-          async
-          src="https://cloud.umami.is/script.js"
-          data-website-id="3eb4248e-2eb6-4750-a0c9-9671b9677822"
-        />
-      </html>
-    </ViewTransitions>
-  );
+	return (
+		<ViewTransitions>
+			<html lang="en">
+				<body
+					className={`${poppinsSans.variable} ${geistMono.variable} antialiased bg-deep-charcoal bg-dither-2 text-off-white`}
+				>
+					<SmoothScrolling>{children}</SmoothScrolling>
+				</body>
+				<Script
+					async
+					src="https://umami.kylewebdev.com/script.js"
+					data-website-id="9e715dff-bb5a-4bc6-b0a8-c50619e1eb81"
+				/>
+			</html>
+		</ViewTransitions>
+	);
 }
